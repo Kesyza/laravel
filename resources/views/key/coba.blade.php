dad8be3b4cdf771282b9a70c0747192683b0a932
Kesyza <kesyza_210@smkassalaambandung.sch.id>
2021-10-14 16:09:18 +0000

    pesan

---
+++ resources/views/key/coba.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Key</title>
+</head>
+
+<body>
+    <hr />
+
+    <header style="text-align: center;">
+    <h1>Yayasan SMK Assalaam Bandung</h1>
+    <img src="a.png" width="200" height="200" style=""/>
+        <h1>Kesyza Andriana Hartuti</h1>
+
+    </header>
+
+    <hr />
+
+    <article style="text-align: center;">
+        <h2>Overview</h2>
+        <p>
+            Hi, saya adalah Kesyza Sekolah di SMK Assalaam Bandung.
+            Saat ini sedang belajar Laravel di kelas
+        </p>
+    </article>
+
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>Skill</th>
+                    <th>Pengalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML (Expert)</li>
+                            <li>CSS (Beginner)</li>
+                            <li>Javascript (Beginner)</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Membuat desain</li>
+                            <li>Banyak Hal</li>
+                            <li>tidak banyak</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright &copy; 2021 Kesyza Andriana Hartuti.</p>
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return '<h1>Halo</h1>' 
+    return '<h1>Halo</h1>'
     .'Selamat datang di web saya<br>'
     .'Laravel, emang keren.';
 });
@@ -41,4 +41,29 @@ Route::get('/prestasi', function () {
 
 Route::get('/penutup', function () {
     return view('/key/penutup');
+});
+
+Route::get('/coba', function () {
+    return view('/key/coba');
+});
+
+Route::get('page/{page?}', function ($hal = 2) {
+    return "Halaman <b>$hal</b>";
+});
+
+Route::get('/pesan/{makanan?}/{minuman?}/{cemilan?}', function ($makanan = null, $minuman = null, $cemilan = null) {
+
+    if($makanan && $minuman && $cemilan != null){
+        return "Anda memesan Makanan = $makanan <br>
+                Anda memesan Minuman = $minuman <br>
+                Anda memesan Cemilan = $cemilan";
+    }else if($makanan && $minuman != null){
+        return "Anda memesan Makanan = $makanan <br>
+                Anda memesan Minuman = $minuman";
+    }else if($makanan || $minuman != null){
+        return $makanan != null ? "Anda memesan makanan : $makanan" : "Anda memesan minuman : $minuman";
+    }else {
+        return "Anda tidak memesan silahkan pulang";
+    }
+
 });
